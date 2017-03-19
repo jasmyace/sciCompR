54ba3949e9d62e58c178b4f2d07a1504fc2f811a
jasmyace <jasmyace@gmail.com>
2017-03-19 17:08:21 -0600

    Make Gauss-Seidel 'vector' way only, i.e., not via matrix considerations.

---
+++ Gauss-Seidel.R
@@ -0,0 +1,48 @@
+gs <- function(A,b,method="v",epsilon=1e-3,maxIters=1000){
+  n <- nrow(A)
+  d <- diag(A)
+  convergence <- FALSE
+  
+  for(j in 1:n){
+    x[j] <- 0
+  }
+  
+  i <- 1
+  
+  repeat{
+    
+    if( (norm < epsilon & i > 2) | i == maxIters ){
+      convergence <- TRUE
+      return(list(x=x,iters=i - 1,norm=norm))
+      break
+    } else {
+      
+      if(method == "v"){
+        
+        xh <- x
+        for(j in 1:n){
+          s <- 0
+          for(k in 1:n){
+            if(k != j){
+              s <- s - A[j,k]*x[k] 
+            }
+          }
+          x[j] <- (s + b[j]) /  d[j]
+        }
+      } else if(method == "m"){
+        #         D <- diagM(A)
+        #         if(i == 1){
+        #           for(j in 1:n){
+        #             x[j] <- 0
+        #           }
+        #         } else {
+        #           xh <- x
+        #           x <- solve(D,(D - A) %*% x + b)
+        #         } 
+      } 
+      norm <- norm(x - xh,"f")
+      cat(paste0("Iteration ",i,":  Norm = ",round(norm,3),", Epsilon = ",epsilon,", Vector = ",t(x),"\n"))
+      i <- i + 1
+    }
+  }
+}
